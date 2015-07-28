12182f003fe94a60685478c39470c6d89dace84c
felipebill@gmail.com <felipebill@gmail.com>
2015-07-28 13:45:42 -0300

    Interface REST

---
+++ src/main/java/rest/UsuarioRest.java
@@ -0,0 +1,38 @@
+package rest;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import service.UsuarioService;
+import entity.Usuario;
+
+@Path("/usuario")
+@RequestScoped
+public class UsuarioRest {
+
+	@EJB
+	private UsuarioService usuarioService;
+
+	@Path("/save")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario save(@QueryParam("nome") String nome,
+			@QueryParam("email") String email,
+			@QueryParam("senha") String senha,
+			@QueryParam("confirmacaoSenha") String confirmacaoSenha) {
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		usuario.setEmail(email);
+		usuario.setSenha(senha);
+		usuario.setConfirmacaoSenha(confirmacaoSenha);
+		usuarioService.saveUsuario(usuario);
+
+		return usuario;
+	}
+
+}
