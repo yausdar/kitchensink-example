31eb3c17a28d17e7f425f665dd0e607a92f3352a
felipebill@gmail.com <felipebill@gmail.com>
2015-08-06 15:31:43 -0300

    Controle de acesso

--- src/main/java/bean/UsuarioBean.java
+++ src/main/java/bean/UsuarioBean.java
@@ -6,10 +6,9 @@ import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
-import entity.Usuario;
-import auth.AuthException;
 import service.UsuarioService;
 import util.Faces;
+import entity.Usuario;
 
 @Named
 @RequestScoped
@@ -51,17 +50,10 @@ public class UsuarioBean {
 	}
 
 	public List<Usuario> getListaUsuario() {
-		try {
-
-			if (this.listaUsuario == null) {
-				this.listaUsuario = usuarioService.getAll();
-			}
-		} catch (AuthException e) {
-			Faces.addError(e.getMessage());
+		if (listaUsuario == null) {
+			listaUsuario = usuarioService.getAll();
 		}
-
 		return this.listaUsuario;
-
 	}
 
 	public void setListaUsuario(List<Usuario> listaUsuario) {
